7247963f3e2930ff5cd4587925d7584a7531ddb6
9lumint <131740436+9lumint@users.noreply.github.com>
2023-07-07 16:42:45 +0900

    [unrated] Title: 귤 고르기, Time: 820.65 ms, Memory: 57.5 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.js
@@ -5,10 +5,11 @@ function solution(k, tangerine) {
     arr[tangerine[i]] = (arr[tangerine[i]] || 0) + 1;
   }
   arr.sort((a, b) => b - a);
-  for (let j = 0; j < arr.length; j++, count++) {
-    if (k <= 0) {
+  for (let j = 0, sum = 0; j < arr.length; j++) {
+    sum += arr[j];
+    count++;
+    if (k <= sum) {
       return count;
     }
-    k -= arr[j];
   }
 }
