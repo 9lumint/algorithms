003ee6108e68f5d3ae878c4d42ddfd639acf2561
9lumint <131740436+9lumint@users.noreply.github.com>
2023-09-18 23:38:37 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.js
@@ -1,9 +1,10 @@
 function solution(a, b, c) {
-    if(a === b && b === c){
-        return 27 * (a ** 6);
-    } else if((a !== b && b !== c) && a !== c){
-        return a + b + c;
+    let sum1 = a + b + c, sum2 = a * a + b * b + c * c, sum3 = a ** 3 + b ** 3 + c ** 3;
+    if((a !== b && b !== c) && a !== c){
+        return sum1;
+    } else if(a === b && b === c){
+        return sum1 * sum2 * sum3;
     } else {
-        return (a + b + c) * (a ** 2 + b ** 2 + c ** 2);
+        return sum1 * sum2;
     }
 }
