9a1f73e3e33984b3345febb55989bfe90df8aad2
9lumint <131740436+9lumint@users.noreply.github.com>
2023-10-31 00:13:00 +0900

    [level unrated] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/unrated/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -4,16 +4,16 @@ function solution(keyinput, board) {
   for (let i = 0; i < keyinput.length; i++) {
     switch (keyinput[i]) {
       case "up":
-        Math.floor(board[1] / 2) < Math.abs(b + 1) ? Math.floor(board[1] / 2) : b++;
+        board[1] / 2 >= b + 1 ? b++ : b;
         break;
       case "down":
-        Math.floor(board[1] / 2) < Math.abs(b - 1) ? Math.floor(-board[1] / 2) : b--;
+        board[1] / 2 >= -b + 1 ? b-- : b;
         break;
       case "right":
-        Math.floor(board[0] / 2) < Math.abs(a + 1) ? Math.floor(board[0] / 2) : a++;
+        board[0] / 2 >= a + 1 ? a++ : a;
         break;
       case "left":
-        Math.floor(board[0] / 2) < Math.abs(a - 1) ? Math.floor(-board[0] / 2) : a--;
+        board[0] / 2 >= -a + 1 ? a-- : a;
         break;
     }
   }
