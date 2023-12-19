eb59e9f29abffd23881276551915ffed326e7c93
9lumint <131740436+9lumint@users.noreply.github.com>
2023-12-20 01:13:15 +0900

    [level 1] Title: 직사각형 별찍기, Time: 43.53 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12969. 직사각형 별찍기/직사각형 별찍기.js
@@ -0,0 +1,10 @@
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", (data) => {
+  const n = data.split(" ");
+  const a = Number(n[0]), b = Number(n[1]);
+  let i = 1;
+  while (i <= b) {
+    console.log("*".repeat(a));
+    i++;
+  }
+});
