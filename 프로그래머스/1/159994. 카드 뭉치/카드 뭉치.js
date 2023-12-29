8bc3632f99538577f31272704d1ee5cd6988cad4
9lumint <131740436+9lumint@users.noreply.github.com>
2023-12-29 22:02:57 +0900

    [level 1] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,8 +1,8 @@
 function solution(cards1, cards2, goal) {
   for (let i = 0; i < goal.length; i++) {
-    if (goal[i] === cards1.slice(0, 1)[0]) {
+    if (goal[i] === cards1[0]) {
       cards1.splice(0, 1);
-    } else if (goal[i] === cards2.slice(0, 1)[0]) {
+    } else if (goal[i] === cards2[0]) {
       cards2.splice(0, 1);
     } else {
       return "No";
