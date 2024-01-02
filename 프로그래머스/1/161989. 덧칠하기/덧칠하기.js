772063575c8dda5ec453e456d0dce864f3cd55a5
9lumint <131740436+9lumint@users.noreply.github.com>
2024-01-03 01:25:57 +0900

    [level 1] Title: 덧칠하기, Time: 1.81 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.js
@@ -1,12 +1,10 @@
 function solution(n, m, section) {
-    let stack = [], answer = 0;
+    let painted = 0, answer = 0;
     for(let i = 0; i < section.length; i++){
-        if(!stack.length || section[i] - stack[0] <= m - 1){
-            stack.push(section[i]);
-        } else if(section[i] - stack[0] > m - 1){
+        if(painted < section[i]){
+            painted = section[i] + m - 1;
             answer++;
-            stack = [section[i]];
         }
     }
-    return answer + 1;
+    return answer;
 }
