cb9ea53b08cc64e0ffea689c6e566b17120eefc5
9lumint <131740436+9lumint@users.noreply.github.com>
2024-01-08 23:58:09 +0900

    [level 1] Title: 명예의 전당 (1), Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,11 +1,8 @@
 function solution(k, score) {
-  const answer = [];
-  const list = [];
-  for (let i = 0; i < score.length; i++) {
-    list.push(score[i]);
-    list.sort((a, b) => b - a);
-    list.length > k ? list.splice(-1) : null;
-    answer.push(list.slice(-1)[0]);
-  }
-  return answer;
+  let answer = [];
+  return score.reduce((a, c) => {
+      answer.push(c);
+      answer = answer.sort((a, b) => b - a).slice(0, k);
+      return [...a, Math.min(...answer)];
+  }, []);
 }
