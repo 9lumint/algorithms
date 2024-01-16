6d74891b61f948947edb6ddae703253b77cef42d
9lumint <131740436+9lumint@users.noreply.github.com>
2024-01-17 00:27:22 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 24.78 ms, Memory: 83.3 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -1,7 +1,3 @@
 function solution(arr) {
-  const answer = [arr[0]];
-  for (let i = 1; i < arr.length; i++) {
-    answer[answer.length - 1] !== arr[i] ? answer.push(arr[i]) : null;
-  }
-  return answer;
+  return arr.filter((e, i) => e !== arr[i + 1]);
 }
