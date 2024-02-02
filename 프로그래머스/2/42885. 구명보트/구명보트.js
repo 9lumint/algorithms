315f7db47a9fbca722062dcd72978318c58d0002
9lumint <131740436+9lumint@users.noreply.github.com>
2024-02-02 23:45:10 +0900

    [level 2] Title: 구명보트, Time: 12.43 ms, Memory: 38.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.js
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -1,14 +1,8 @@
 function solution(people, limit) {
-    people.sort((a, b) => a - b);
-    let answer = 0;
-    while(people.length){
-        if(people[0] + people[people.length - 1] <= limit){
-            people.pop();
-            people.shift();
-        } else {
-            people.pop();
-        }
-        answer++;
-    }
-    return answer;
+  people.sort((a, b) => a - b);
+  let count = 0;
+  for (let i = people.length - 1; count < i; i--) {
+    people[i] + people[count] <= limit ? count++ : null;
+  }
+  return people.length - count;
 }
